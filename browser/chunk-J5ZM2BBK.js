d1106e48f361c9e175f8db1252f1417465ffcbd5
Leonardo Salge <lferreirasalge@gmail.com>
2025-04-30 15:01:02 -0300

    Auto-generated commit

---
+++ browser/chunk-J5ZM2BBK.js
@@ -0,0 +1,1 @@
+import{Hb as p,Xa as n,pb as o,qb as i}from"./chunk-FY5ENRAQ.js";var m=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-news"]],decls:2,vars:0,template:function(t,r){t&1&&(o(0,"p"),p(1,"news works!"),i())},encapsulation:2})}return e})();export{m as NewsComponent};

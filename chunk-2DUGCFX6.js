64b0101a7d9cfe89f8c3f0af9f48253542b2a77d
Leonardo Salge <lferreirasalge@gmail.com>
2025-05-06 22:17:21 -0300

    Auto-generated commit

---
+++ chunk-2DUGCFX6.js
@@ -0,0 +1,1 @@
+import{Jb as a,Za as n,rb as o,sb as i}from"./chunk-KW4OMIO2.js";var m=(()=>{class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-donations"]],decls:2,vars:0,template:function(e,s){e&1&&(o(0,"p"),a(1,"donations works!"),i())},encapsulation:2})}return t})();export{m as DonationsComponent};
